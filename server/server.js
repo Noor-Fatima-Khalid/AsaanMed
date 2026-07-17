e8b95896ee321938135d2125e20f806661320c7c
Noor <h.noorfatima7@gmail.com>
2026-07-17 23:21:41 +0500

    backend deployment fixes

--- server/server.js
+++ server/server.js
@@ -19,8 +19,8 @@ connectCloudinary()
 app.use(express.json())
 app.use(cors({
     origin: [
-        "https://asaanmed-client.vercel.app",
-        "https://asaanmed-admin.vercel.app"
+        "https://asaan-med.vercel.app/",
+        "https://asaan-med-admin.vercel.app/"
     ],
     credentials: true
 }));
@@ -32,6 +32,10 @@ app.use("/api/admin", adminRouter)
 app.use("/api/doctor", doctorRouter)
 app.use("/api/payment", paymentRouter)
 
-app.listen(port, ()=>
-    console.log("server running")
-)
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log("Server running");
+  });
+}
+
+export default app;
